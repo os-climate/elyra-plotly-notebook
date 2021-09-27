1d4312ac73af481624be30bd70f587b6273441c3
MichaelTiemannOSC <mtiemann@os-climate.org>
2021-09-27 18:41:43 +0000

    Create notebook example
    Initial commit of Plotly notebook example.  In this initial commit, we need to run the OS-C-pip.sh shell script and re-start the JupyterLab instance (by refreshing the notebook page in the browser and restarting the kernel).  This example passed its tests at check-in using the above initialization procedure.

---
+++ OS-C-pip.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Until we can properly support JupyterLab, use Plotly version 4
+pip3 install ipywidgets plotly
+
+# For Plotly version 4, deal with plotlywidget extension (which is automatic in Plotly 5 and JupyterLab)
+# jupyter nbextension install plotlywidget --user --py
+# jupyter nbextension enable plotlywidget --user --py
+
+# Establish basic geo libraries for Physical Risk
+pip3 install pandas geopandas matplotlib affine snuggs ipyleaflet
+
+# Activate the ipyleaflet extensions where we can see them
+# Check Edit->nbextensions config menu item to see that ipyleaflet extension is visible
+# /opt/app-root/miniconda3/bin/jupyter nbextension install ipyleaflet --user --py
+# /opt/app-root/miniconda3/bin/jupyter nbextension enable ipyleaflet --user --py
+
+# Install known-good rasterio from PyPi
+pip3 install rasterio
